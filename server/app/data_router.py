09140961db27d005e007f73910aa2e044367ab82
Sean Breckenridge <seanbrecke@gmail.com>
2022-02-03 21:41:33 -0800

    add basic data endpoint

--- server/app/data_router.py
+++ server/app/data_router.py
@@ -1,3 +1,77 @@
-from fastapi import APIRouter
+from typing import Optional, Dict, Any, List, cast
+from enum import Enum
+import pickle
+
+import orjson
+
+from fastapi import APIRouter, Depends, Query
+from pydantic import validator
+from sqlmodel import Session, Field, select
+
+
+from app.db import get_db, FeedModel, FeedBase
 
 router = APIRouter()
+
+
+class FeedRead(FeedBase):  # type: ignore
+    # parse from the backend to data structures
+    tags: List[str] = Field(default_factory=list)
+    data: Dict[str, Any] = Field(default_factory={})
+
+    @validator("tags", pre=True)
+    def parse_tags(cls, v: Any) -> List[str]:
+        return cast(List[str], orjson.loads(v))
+
+    @validator("data", pre=True)
+    def parse_data(cls, v: Any) -> Dict[str, Any]:
+        if v is None:
+            return {}
+        else:
+            return cast(Dict[str, Any], pickle.loads(v))
+
+
+class OrderBy(Enum):
+    score = "score"
+    when = "when"
+
+
+class Sort(Enum):
+    asc = "asc"
+    ascending = asc
+    desc = "desc"
+    descending = desc
+
+
+@router.get("/", response_model=List[FeedRead])
+async def data(
+    offset: int = 0,
+    limit: int = Query(default=100, lte=100),
+    order_by: OrderBy = Query(default=OrderBy.when),
+    sort: Sort = Query(default=Sort.desc),
+    query: Optional[str] = Query(default=None, min_length=2),
+    title: Optional[str] = Query(default=None, min_length=2),
+    creator: Optional[str] = Query(default=None, min_length=2),
+    subtitle: Optional[str] = Query(default=None, min_length=2),
+    session: Session = Depends(get_db),
+) -> List[FeedRead]:
+    with session:
+        stmt = select(FeedModel)
+        if query is None:
+            if title:
+                stmt = stmt.filter(FeedModel.title.ilike(f"%{title}%"))  # type: ignore
+            if creator:
+                stmt = stmt.filter(FeedModel.creator.ilike(f"%{creator}%"))  # type: ignore
+            if subtitle:
+                stmt = stmt.filter(FeedModel.subtitle.ilike(f"%{subtitle}%"))  # type: ignore
+        else:
+            stmt = stmt.filter(
+                (FeedModel.title.ilike(f"%{query}"))  # type: ignore
+                | (FeedModel.creator.ilike(f"%{query}%"))  # type: ignore
+                | (FeedModel.creator.ilike(f"%{query}%"))  # type: ignore
+            )
+        order_field = FeedModel.when if order_by == OrderBy.when else FeedModel.score
+        order_func = order_field.desc() if sort == Sort.desc else order_field.asc()  # type: ignore
+        stmt = stmt.order_by(order_func).offset(offset).limit(limit)
+        items: List[FeedModel] = list(session.exec(stmt))
+        return items
--- server/app/db.py
+++ server/app/db.py
@@ -7,10 +7,9 @@ from my_feed.log import logger
 from app.settings import settings
 
 
-class FeedModel(SQLModel, table=True):  # type: ignore
-    # basics
-    id: int = Field(index=True, primary_key=True)
-    model_id: str = Field(index=True)
+# base non-table sql model
+class FeedBase(SQLModel):  # type: ignore
+    model_id: str
     ftype: str  # feed item type
     title: str
     score: Optional[float] = Field(default=None)
@@ -21,9 +20,6 @@ class FeedModel(SQLModel, table=True):  # type: ignore
     part: Optional[int] = Field(default=None)
     subpart: Optional[int] = Field(default=None)
     collection: Optional[str] = Field(default=None)
-    # store JSON as strings, these are only used on the frontend anyways
-    tags: str = Field(default=r"[]")  # List[str]
-    data: Optional[bytes] = Field(default=None)  # Dict[str, Any]
 
     # dates
     when: datetime
@@ -32,6 +28,15 @@ class FeedModel(SQLModel, table=True):  # type: ignore
     # urls
     image_url: Optional[str] = Field(default=None)
     url: Optional[str] = Field(default=None)
+
+
+# feedbase, with an ID/table
+class FeedModel(FeedBase, table=True):  # type: ignore
+    id: int = Field(index=True, primary_key=True)
+
+    # store JSON as strings, these are only used on the frontend anyways
+    tags: str = Field(default=r"[]")  # List[str]
+    data: Optional[bytes] = Field(default=None)  # Dict[str, Any]
 
 
 feed_engine = create_engine(
