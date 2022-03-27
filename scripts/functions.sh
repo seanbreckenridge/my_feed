76fd8f8dc2739d1c50d58ecc36722df4bdd9c904
Sean Breckenridge <seanbrecke@gmail.com>
2022-03-27 03:10:19 -0700

    add functions file with some shorthands

---
+++ scripts/functions.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env zsh
+
+feed_describe_album_score() {
+	jq -r '.[] | "\(.image_url)|\(.title)|\(.subtitle)|\(.score)/10"' -r
+}
+
+feed_describe_album() {
+	jq -r '.[] | "\(.image_url)|\(.title)|\(.subtitle)"' -r
+}
+
+feed_img() {
+	jq -r '.[] | "\(.image_url)"'
+}
+
+feed_favorite_albums() {
+	curl -sL 'https://sean.fish/feed_api/data/?offset=0&limit=100&order_by=score&sort=desc&ftype=album' | jq
+}
+
+feed_recent_albums() {
+	local recent="${1?:provide recent duration}"
+	local after_epoch="$(date -d "-$recent" +%s)"
+	curl -sL 'https://sean.fish/feed_api/data/?offset=0&limit=100&order_by=score&sort=desc&ftype=album' |
+		jq --arg AFTER "${after_epoch}" '.[] | select(.when >= ($AFTER | tonumber))' |
+		jq -s
+}
