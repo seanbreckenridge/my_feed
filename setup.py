da2f93bd2fc2c33796e12e81c4725270846f7cb7
Sean Breckenridge <seanbrecke@gmail.com>
2023-05-06 19:22:37 -0700

    setup: add requirements to pip install

--- setup.py
+++ setup.py
@@ -1,4 +1,7 @@
+from pathlib import Path
 from setuptools import setup, find_namespace_packages  # type: ignore[import]
+
+reqs = Path("requirements.txt").read_text().strip().splitlines()
 
 
 pkg = "my_feed"
@@ -16,6 +19,7 @@ def main() -> None:
         zip_safe=False,
         description="A personal feed/website",
         package_data={pkg: ["py.typed"]},
+        install_requires=reqs,
         license="MIT",
         entry_points={"console_scripts": ["my_feed = my_feed.__main__:main"]},
         version="0.1.0",
